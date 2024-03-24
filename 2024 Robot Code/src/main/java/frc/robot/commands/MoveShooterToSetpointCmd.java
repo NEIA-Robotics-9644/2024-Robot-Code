8581b4efe5e56e5abd57f7cb5d24134d47451b9b
samjackson24816 <133062281+samjackson24816@users.noreply.github.com>
2024-03-24 18:42:21 -0400

    Clarified MoveShooterToSetpointCmd

--- 2024 Robot Code/src/main/java/frc/robot/commands/MoveShooterToSetpointCmd.java
+++ 2024 Robot Code/src/main/java/frc/robot/commands/MoveShooterToSetpointCmd.java
@@ -3,6 +3,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
+/*
+ * Command to move the shooter to a setpoint
+ * Never ends, so it should be stopped by the system calling it
+ */
 public class MoveShooterToSetpointCmd extends Command {
     
 
