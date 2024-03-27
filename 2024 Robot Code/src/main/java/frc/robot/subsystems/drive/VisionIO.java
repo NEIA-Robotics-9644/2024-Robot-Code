1e59c404024eb0d6b758db761258abdc8a271ea1
samjackson24816 <133062281+samjackson24816@users.noreply.github.com>
2024-03-26 22:17:18 -0400

    Got rid of unneeded imports

--- 2024 Robot Code/src/main/java/frc/robot/subsystems/drive/VisionIO.java
+++ 2024 Robot Code/src/main/java/frc/robot/subsystems/drive/VisionIO.java
@@ -8,8 +8,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 
 import java.util.Optional;
