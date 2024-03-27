6e70292f6121fa89e7646c57ab0b4912b5bf48fe
samjackson24816 <133062281+samjackson24816@users.noreply.github.com>
2024-03-27 17:44:17 -0400

    Got rid of unused inputs

--- 2024 Robot Code/src/main/java/frc/robot/RobotContainer.java
+++ 2024 Robot Code/src/main/java/frc/robot/RobotContainer.java
@@ -7,17 +7,13 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
 import com.ctre.phoenix6.Utils;
-import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import frc.robot.Constants.DriveConstants;
@@ -25,7 +21,6 @@ import frc.robot.Constants.Modes;
 import frc.robot.commands.ClimberCmd;
 import frc.robot.commands.JoystickDriveCmd;
 import frc.robot.commands.MoveShooterToBottomAndResetCmd;
-import frc.robot.commands.MoveShooterToSetpointCmd;
 import frc.robot.commands.RunSourceIntakeCmd;
 import frc.robot.commands.ShootWhenReadyCmd;
 import frc.robot.commands.SpinShooterWheelsCmd;
@@ -36,7 +31,6 @@ import frc.robot.subsystems.drive.SwerveDriveSubsystem;
 import frc.robot.subsystems.shooter.feederWheel.FeederWheelIOSim;
 import frc.robot.subsystems.shooter.feederWheel.FeederWheelIOSparkMax;
 import frc.robot.subsystems.shooter.shooterAngle.ShooterAngleIOSparkMax;
-import frc.robot.subsystems.shooter.shooterWheel.ShooterWheelIOSparkMax;
 import frc.robot.subsystems.shooter.shooterWheel.ShooterWheelIOTalonFX;
 import frc.robot.subsystems.shooter.shooterWheel.ShooterWheelIOSim;
 import frc.robot.subsystems.shooter.shooterAngle.ShooterAngleIOSim;
