f9bd7da4a4d0ef5b82e090b4c0621fa98dd9974e
samjackson24816 <133062281+samjackson24816@users.noreply.github.com>
2024-03-30 09:59:42 -0400

    WPI Practice Match 4 Code

--- 2024 Robot Code/src/main/java/frc/robot/Constants.java
+++ 2024 Robot Code/src/main/java/frc/robot/Constants.java
@@ -157,7 +157,7 @@ public final class Constants {
                 private static final int kFrontLeftDriveMotorId = 1;
                 private static final int kFrontLeftSteerMotorId = 2;
                 private static final int kFrontLeftEncoderId = 3;
-                private static final double kFrontLeftEncoderOffset = 0.20947265625;
+                private static final double kFrontLeftEncoderOffset = 0.2021484375;
 
                 private static final double kFrontLeftXPosInches = 11.25;
                 private static final double kFrontLeftYPosInches = 11.25;
@@ -166,7 +166,7 @@ public final class Constants {
                 private static final int kFrontRightDriveMotorId = 4;
                 private static final int kFrontRightSteerMotorId = 5;
                 private static final int kFrontRightEncoderId = 6;
-                private static final double kFrontRightEncoderOffset = 0.208251953125;
+                private static final double kFrontRightEncoderOffset = 0.197509765625;
 
                 private static final double kFrontRightXPosInches = 11.25;
                 private static final double kFrontRightYPosInches = -11.25;
@@ -175,7 +175,7 @@ public final class Constants {
                 private static final int kBackLeftDriveMotorId = 7;
                 private static final int kBackLeftSteerMotorId = 8;
                 private static final int kBackLeftEncoderId = 9;
-                private static final double kBackLeftEncoderOffset = -0.205078125;
+                private static final double kBackLeftEncoderOffset = -0.215576171875;
 
                 private static final double kBackLeftXPosInches = -11.25;
                 private static final double kBackLeftYPosInches = 11.25;
@@ -184,7 +184,7 @@ public final class Constants {
                 private static final int kBackRightDriveMotorId = 10;
                 private static final int kBackRightSteerMotorId = 11;
                 private static final int kBackRightEncoderId = 12;
-                private static final double kBackRightEncoderOffset = 0.26025390625;
+                private static final double kBackRightEncoderOffset = 0.269287109375;
 
                 private static final double kBackRightXPosInches = -11.25;
                 private static final double kBackRightYPosInches = -11.25;
