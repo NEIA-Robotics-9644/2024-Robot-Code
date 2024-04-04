a26e7449449c8e4f207999a82fed3ca4e9dcd13b
samjackson24816 <133062281+samjackson24816@users.noreply.github.com>
2024-04-04 08:03:17 -0400

    Added test command hook

--- 2024 Robot Code/src/main/java/frc/robot/RobotContainer.java
+++ 2024 Robot Code/src/main/java/frc/robot/RobotContainer.java
@@ -96,8 +96,8 @@ public class RobotContainer {
           new FeederWheelIOSparkMax(23),
           new ShooterAngleIOSparkMax(24, 25),
           new NoteSensorIORoboRio(),
-          new double[] { 0, 25, 52, 70},
-          new double[] { 1, 0.4, 1, 0.4},
+          new double[] { 0, 25, 48, 52},
+          new double[] { 1, 0.4, 1, 1},
           new double[] { 1, 0.4, 1, 0.4}
       );
 
@@ -115,8 +115,8 @@ public class RobotContainer {
           new FeederWheelIOSim(),
           new ShooterAngleIOSim(),
           new NoteSensorIOSim(),
-          new double[] { 0, 25, 52, 70},
-          new double[] { 1, 0.4, 1, 0.4},
+          new double[] { 0, 25, 48, 52},
+          new double[] { 1, 0.4, 1, 1},
           new double[] { 1, 0.4, 1, 0.4}
       );
 
@@ -256,6 +256,12 @@ public class RobotContainer {
     
 
     return autoCreator.createAuto();
+  }
+
+  public Command getTestCommand() {
+    return Commands.run(() -> {
+      System.out.println("Test Command");
+    });
   }
 
     
