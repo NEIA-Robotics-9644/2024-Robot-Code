b2a512e18eef191b8a75654fd225bbb008780216
samjackson24816 <133062281+samjackson24816@users.noreply.github.com>
2024-07-22 12:14:45 -0400

    Adding before I delete my local

---
+++ 2024 Robot Code/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024 Robot Code";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 189;
+  public static final String GIT_SHA = "0c7bd0b6ba0840477b23009822f245465a2c0e7a";
+  public static final String GIT_DATE = "2024-04-05 22:05:11 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-04-19 10:15:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1713536133414L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
